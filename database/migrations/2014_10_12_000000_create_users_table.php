a9ef4e9e271680270bc00658e2cd0cf48d410fb5
Dilouette <dayo@acclio.com>
2022-06-24 23:40:38 +0100

    user table fk fix

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,14 +18,14 @@ return new class extends Migration {
             $table->string('username');
             $table->string('firstname');
             $table->string('lastname');
-            $table->integer('department_id')->nullable();
+            $table->unsignedInteger('department_id')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('remember_token', 100)->nullable();
             $table->boolean('reset_login');
             $table->boolean('first_login');
             $table->dateTime('last_login');
-            $table->integer('designation_id')->nullable();
+            $table->unsignedInteger('designation_id')->nullable();
             $table->boolean('is_active');
 
             $table->timestamps();
